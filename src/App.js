32ed35f7a119bfed2f2aedd210ce4afeb6f24b04
joeychun <joey.s.chun@gmail.com>
2022-07-27 01:15:43 +0900

    deleted unused stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route, Navigate, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import TodoList from './containers/TodoList/TodoList';
 import TodoDetail from './components/TodoDetail/TodoDetail';
 import NewTodo from './containers/TodoList/NewTodo/NewTodo';
@@ -9,15 +8,15 @@ import NewTodo from './containers/TodoList/NewTodo/NewTodo';
 function App() {
   return (
     <BrowserRouter>
-    <div className='App'>
-      <Routes>
-        <Route path='/todos' exact element={<TodoList title='My TODOs!' />} />
-        <Route path='/todos/:id' exact element={<TodoDetail />} />
-        <Route path='/new-todo' exact element={<NewTodo />} />
-        <Route path='/' element={<Navigate replace to='/todos' />} />
-        <Route path='*' element={<h1>Not Found</h1>} />
-      </Routes>
-    </div>
+      <div className='App'>
+        <Routes>
+          <Route path='/todos' exact element={<TodoList title='My TODOs!' />} />
+          <Route path='/todos/:id' exact element={<TodoDetail />} />
+          <Route path='/new-todo' exact element={<NewTodo />} />
+          <Route path='/' element={<Navigate replace to='/todos' />} />
+          <Route path='*' element={<h1>Not Found</h1>} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
